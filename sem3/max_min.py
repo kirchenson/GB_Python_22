167b33ebb3c9dee663182ac9f0894a1f30ed2d03
kirchenson <84853195+kirchenson@users.noreply.github.com>
2022-11-02 23:06:08 +0300

    Update max_min.py

--- sem3/max_min.py
+++ sem3/max_min.py
@@ -1,1 +1,12 @@
+# 1. Задайте строку из набора чисел. Напишите программу, которая покажет
+# большее и меньшее число. В качестве символа-разделителя используйте пробел.
+
+def max_min(str_num):
+    str_num = str_num.split(' ')
+    str_num = [int(i) for i in str_num]
+    return max(str_num), min(str_num)
+
+str_num = input('введите числа через пробел: ')
+ans = max_min(str_num)
+print(f'Максимальное число: {ans[0]}\nМинимальное число: {ans[1]}')
 
